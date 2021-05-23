23042e447fc36dc3bd98790eaa71729584f57d8f
Kazi Nizamul <kazinizamul.work@hotmail.com>
2021-05-23 23:09:12 +0530

    findIndex

---
+++ JS/Array/functions/findIndex.js
@@ -0,0 +1,22 @@
+// same as find function 
+
+var arr = [{ a: 1 }, { a: 2 }];
+
+console.log(
+
+
+	arr.findIndex(function match(v) {            // callback func is being used
+		return v.a > 1;
+    })
+    
+
+);
+
+// output : 1
+
+// -----------------------------------
+/**
+ *      arr.findIndex( (v) =>  {                 // easier way
+ *          return (v.a > 1)
+ *      });
+ */
