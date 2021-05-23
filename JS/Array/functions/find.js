df7788effcb2f69d1446764b853e41626b71368e
Kazi Nizamul <kazinizamul.work@hotmail.com>
2021-05-23 23:07:26 +0530

    Update find.js

--- JS/Array/functions/find.js
+++ JS/Array/functions/find.js
@@ -4,17 +4,17 @@ console.log(
 	
     arr.find(function match(v) {                  // callback func is being used
 	return v.a > 1;
-
-		
-	})
+    })
+	
+	
 );
 
 // output : { a: 2 }
 
 
 // -----------------------------------
-    /**
-     *      arr.find( (v) =>  {                 // easier way
-     *          return (v.a > 1)
-     *      });
-     */
+/**
+*      arr.find( (v) =>  {                 	// easier way
+*          return (v.a > 1)
+*      });
+*/
