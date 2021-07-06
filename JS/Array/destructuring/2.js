3bc29ec9c1d4f225512a5fbf5182b45b4d2a1634
Kazi Nizamul <kazinizamul.work@hotmail.com>
2021-07-06 14:26:53 +0530

    Commit

--- JS/Array/destructuring/2.js
+++
@@ -1,8 +0,0 @@
-function data([first, second, third] = []) {
-  // ...
-}
-
-// jo bhi ayega data ke andar, usme se first, second, third ko lelo
-
-// aurr agar data ke andar kuch na aa rha, toh, empty arr, andar ayega data ka
-// isiliye "= []" kiya gya h
--- JS/keywords/this.js
+++
@@ -1,9 +0,0 @@
-/**
- *  "this" is created by JS engine
- *   into the global scope
- * 
- * 
- *      For browsers        :  this === window
- *     
- *  
- */
