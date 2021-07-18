4da69b0a585b1f894425a5506518b5157b0b2f1f
Kazi Nizamul <kazinizamul.work@hotmail.com>
2021-07-18 20:27:06 +0530

    commit

--- JS/OPTIMIZATION/benchmarks.js
+++ JS/OPTIMIZATION/benchmarks.js
@@ -3,20 +3,20 @@ const { performance, PerformanceObserver } = require('perf_hooks');
 
 
 /* ---------------------------------- setup --------------------------------- */
-let iterations = 1000000000;
-const a = 10;
-const b = 20;
+// let iterations = 1000000000;
+// const a = 10;
+// const b = 20;
 
-const add = (x, y) => x + y;
+// const add = (x, y) => x + y;
 
 performance.mark('start');  // start pivot 
 
 /* ---------------------- %NeverOptimizeFunction(add); ---------------------- */
 /* --------- this is to check, how much time it'll take without opt --------- */
 
-while (iterations--) {
-    add(a, b);
-}
+// while (iterations--) {
+//     add(a, b);
+// }
 
 performance.mark('end');   // end pivot
 
