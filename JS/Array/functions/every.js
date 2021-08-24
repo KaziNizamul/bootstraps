8a44d63a47634c9b7c33d035a4403e8aa482f6a1
Kazi Nizamul <kazinizamul.work@hotmail.com>
2021-08-24 09:54:49 +0530

    Create every.js

---
+++ JS/Array/functions/every.js
@@ -0,0 +1,18 @@
+
+// condition likhenge
+// koi agar condition pass nai krta toh, true false mein result ayega
+
+[1,2,3].every(elem => (
+    elem >= 1
+))                          // false
+
+
+
+
+
+
+
+
+
+
+
