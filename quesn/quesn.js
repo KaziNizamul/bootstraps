38bd2ed0524215f22805a6923e8cccfaf07421f9
Kazi Nizamul <kazinizamul.work@hotmail.com>
2021-08-25 15:30:30 +0530

    Update quesn.js

--- quesn/quesn.js
+++ quesn/quesn.js
@@ -46,3 +46,4 @@ n. polyfills :
 
 
 more resource to compile : https://soniapahuja75.medium.com/frontend-interview-in-paytm-2-4yrs-exp-1f9d47ce0e9f
+https://leetcode.com/discuss/interview-question/742791/front-end-interview-questions/631397
