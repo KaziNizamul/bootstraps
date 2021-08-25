b8f23e983103e83d07553c346fd69306b5ef62d9
Kazi Nizamul <kazinizamul.work@hotmail.com>
2021-08-25 09:59:48 +0530

    questions

---
+++ quesn/quesn.js
@@ -0,0 +1,45 @@
+
+// questions that are codable in text editors
+
+1. reverse a string
+2. anagram
+
+
+// core js questions
+1. hoisting
+2. closures
+3. prototypes inheritance
+5. call bind apply + code
+
+6. currying + code
+
+7. promises + code
+
+8. generators + code
+9. iterators + code
+
+
+
+
+
+// Js coding quesn
+1. debouncing and throttling + code
+2.
+.
+.
+n. polyfills :
+    1. Object.assign
+    2. Map
+    
+
+
+// Advanced rounds quesn : 
+1. [backend] What is Server side Rendering and why do we need it ?
+    
+2. [html] Without Js, if you click on page corner then any input box  present should be focussed.How you can do that ?
+    
+3. [js] Pure functions
+4. [js] Composition in Js
+5. [js] advancement of box while entering otp 
+
+6. [webpack] How webpack works internally.
