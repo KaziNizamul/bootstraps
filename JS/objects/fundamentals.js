176cefc92fd730557bd2f8a2569a636f54859b05
Kazi Nizamul <kazinizamul.work@hotmail.com>
2021-09-09 10:16:36 +0530

    Update fundamentals.js

--- JS/objects/fundamentals.js
+++ JS/objects/fundamentals.js
@@ -8,3 +8,28 @@
 // console.log(obj) -> {2: 'b', 3: 'a', 5: '5', f: 'f'}
 // meaning : integers agar hai, toh sorted order mein print ho lenge, unke baad, dusre kuch ayega 
 
+
+
+// --------------------------------------------2. creating a clone of obj (deepcopy) : --------------------------------------
+/** 
+    1. manually :
+    
+         let user = { name : 'user', age : 30 } 
+         let clone = { }
+         
+         for(let key in user) {
+              clone[key] = user[key]
+         }
+        
+
+    2. Object.assign(clone, {name, age})
+
+
+    3.  JSON.parse(JSON.stringify(obj1))
+  
+    4. loadash's :          _.cloneDeep(obj1)
+ 
+
+
+
+*/
