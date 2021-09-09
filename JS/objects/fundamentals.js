dd8c555a020b4e8c5e339b50279f1a572d7c221b
Kazi Nizamul <kazinizamul.work@hotmail.com>
2021-09-09 09:46:34 +0530

    Create fundamentals.js

---
+++ JS/objects/fundamentals.js
@@ -0,0 +1,10 @@
+
+ // --------------------------------------------1. ORDER of printing object : --------------------------------------
+
+ let obj = {
+    '3':'a', '2':'b','f':'f', '5':'5'
+}
+
+// console.log(obj) -> {2: 'b', 3: 'a', 5: '5', f: 'f'}
+// meaning : integers agar hai, toh sorted order mein print ho lenge, unke baad, dusre kuch ayega 
+
